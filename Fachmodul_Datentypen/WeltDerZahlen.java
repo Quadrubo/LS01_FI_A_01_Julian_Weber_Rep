f6113b9621e29578a435d2361065a3d86da63496
Quadrubo <julweb61@gmail.com>
2021-01-05 14:52:36 +0100

    WeltderZahlen

---
+++ Fachmodul_Datentypen/WeltDerZahlen.java
@@ -0,0 +1,79 @@
+package Fachmodul_Datentypen;
+
+/**
+  *   Aufgabe:  Recherechieren Sie im Internet !
+  * 
+  *   Sie dÃ¼rfen nicht die Namen der Variablen verÃ¤ndern !!!
+  *
+  *   Vergessen Sie nicht den richtigen Datentyp !!
+  *
+  *
+  * @version 1.0 from 05.01.2021
+  */
+
+public class WeltDerZahlen {
+
+    public static void main(String[] args) {
+
+    /*  *********************************************************
+
+    Zuerst werden die Variablen mit den Werten festgelegt!
+
+    *********************************************************** */
+    // Im Internet gefunden ?
+    // Die Anzahl der Planeten in unserem Sonnesystem                    
+    int anzahlPlaneten =  8;
+
+    // Anzahl der Sterne in unserer MilchstraÃŸe
+    long anzahlSterne = 250000000000l;
+
+    // Wie viele Einwohner hat Berlin?
+    int bewohnerBerlin = 3769000;
+
+    // Wie alt bist du?  Wie viele Tage sind das?
+
+    short alterTage = 6590;
+
+    // Wie viel wiegt das schwerste Tier der Welt?
+    // Schreiben Sie das Gewicht in Kilogramm auf!
+    int gewichtKilogramm = 200000;
+
+    // Schreiben Sie auf, wie viele kmÂ² das grÃ¶ÃŸte Land er Erde hat?
+    long flaecheGroessteLand = 17098242;
+
+    // Wie groÃŸ ist das kleinste Land der Erde?
+
+    float flaecheKleinsteLand = 0.44f;
+
+
+
+
+    /*  *********************************************************
+
+    Programmieren Sie jetzt die Ausgaben der Werte in den Variablen
+
+    *********************************************************** */
+
+    System.out.println("Anzahl Planeten: " + anzahlPlaneten);
+
+    System.out.println("Anzahl Sterne: " + anzahlSterne);
+
+    System.out.println("Einwohner Berlin: " + bewohnerBerlin);
+
+    System.out.println("Alter Tage: " + alterTage);
+
+    System.out.println("schwerstes Tier: " + gewichtKilogramm);
+
+    System.out.println("groesstes Land: " + flaecheGroessteLand);
+
+    System.out.println("kleinstes Land: " + flaecheKleinsteLand);
+
+
+
+
+    System.out.println(" *******  Ende des Programms  ******* ");
+
+    }
+}
+  
+  
