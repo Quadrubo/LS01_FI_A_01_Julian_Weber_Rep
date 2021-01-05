29f2d6b8e8f73f79dca167fceadfc0580bb2e3f7
Quadrubo <julweb61@gmail.com>
2021-01-05 14:10:01 +0100

    AB_Konsolenausgabeformatierung/Aufgabe_1

---
+++ Fachmodul_Konsolen_Ein_und_Ausgaben/AB_Konsolenausgabeformatierung/Aufgabe_1.java
@@ -0,0 +1,10 @@
+package Fachmodul_Konsolen_Ein_und_Ausgaben.AB_Konsolenausgabeformatierung;
+
+public class Aufgabe_1 
+{
+ 
+       public static void main (String[] args)
+       {
+            System.out.printf("  %1$s%1$s  \n%1$s    %1$s\n%1$s    %s\n  %1$s%1$s  ", "*");
+       }
+}
